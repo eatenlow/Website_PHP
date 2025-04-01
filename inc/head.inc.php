445cb6199c24d1ab35096ef60dbac4d9be71dafc
Hongruiiii <2401880@sit.singaporetech.edu.sg>
2025-04-01 23:08:34 +0800

    removed title from head.inc.php

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -1,19 +1,18 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Home | PetAdopt</title>
 <link rel="stylesheet" href="css/styles.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
 <!--Bootstrap JS-->
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-crossorigin="anonymous"> </script>
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"> </script>
 <!-- <link rel="stylesheet" href="css/main.css">
 
 <script defer src="./JS/main.js"></script> -->
 <?php
-if(isset($_SESSION)){
+if (isset($_SESSION)) {
     if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
         echo $_SESSION['LAST_ACTIVITY'];
 
